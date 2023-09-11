db3f540b8f7e60f9204a9ebe039efa999789e6a0
SalfaBella <salfabella64@gmail.com>
2023-09-11 08:01:14 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,43 @@
+<div class="footer footer1">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-md-offset-4">
+            <img src="img/Logo.png" width="72" height="72" />
+            <h4 class="white">Dinas Kesehatan Kota Semarang</h4>
+			<h3 class="white">Indonesia</h3>
+          <ul class="list-inline">
+            <li><a href="" class="link-footer">Beranda</a></li>
+            <li><a href="" class="link-footer">Petunjuk Penggunaan</a></li>
+            <li><a href="" class="link-footer">Tentang</a></li>
+          </ul>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal -->
+<div class="modal fade" id="about" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">About</h4>
+      </div>
+      <div class="modal-body">
+        <h4>SISTEM INFORMASI DINAS KESEHATAN KOTA SEMARANG</h4>
+        <p> Sistem ini memuat data data penyebaran penyakit per wilayah di daerah Semarang</p>
+        <p> </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-hover-dropdown.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/datatable-bootstrap.js"></script>
+    
+  </body>
+</html>
